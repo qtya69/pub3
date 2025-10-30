01feb5a143d95f2104dcbb28a2d52c0e039dcd30
kop <traxlerkoppany@gmail.com>
2025-10-30 12:12:00 +0100

    drink komponens create muvelet felig ok

--- src/app/drink/drink.component.ts
+++ src/app/drink/drink.component.ts
@@ -1,21 +1,44 @@
 import { Component } from '@angular/core';
 import { DrinkapiService } from '../shared/drinkapi.service';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-drink',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './drink.component.html',
   styleUrl: './drink.component.css'
 })
 export class DrinkComponent {
 
+  drinkForm!: any;
   drinks: any;
+  addMode=true;
 
-  constructor(private api: DrinkapiService){}
+  constructor(private api: DrinkapiService, private build:FormBuilder){}
 
   ngOnInit() {
     this.getDrinks();
+    this.drinkForm = this.build.group({
+      id: [''],
+      price: [''],
+      drink: [''],
+      amount: [''],
+      type: [''],
+      package: ['']
+    })
+  }
+
+  save(){
+    console.log("Mentés árnyékeljárás...")
+    console.log(this.drinkForm.value);
+    this.api.createDrink$(this.drinkForm.value).subscribe({
+      next: (result) => {
+        console.log(result);
+        this.drinkForm.reset();
+        this.getDrinks();
+      }
+    })
   }
 
   getDrinks() {
